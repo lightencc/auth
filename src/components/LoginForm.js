a07a85f64e9deee60c9739b81f21cec59de1c6ab
lightencc <chen_x22@yeah.net>
2017-03-02 22:18:30 +0800

    show spinner and hide login button when loading

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
 import firebase from 'firebase';
-import { Button, Card, CardSection, Input } from './common';
+import { Button, Card, CardSection, Input, Spinner } from './common';
 
 class LoginForm extends Component {
 
-    state = { email: '', password: '' };
+    state = { email: '', password: '', error: '', loading: false };
 
     onButtonPress() {
         const { email, password } = this.state;
 
-        this.setState({ error: '' });
+        this.setState({ error: '', loading: true });
 
         firebase.auth().signInWithEmailAndPassword(email, password)
             .catch(() => {
@@ -19,6 +19,18 @@ class LoginForm extends Component {
                         this.setState({ error: 'Authentication Failed.' });
                     });
             });
+    }
+
+    renderButton() {
+        if (this.state.loading) {
+            return <Spinner size="small" />;
+        }
+
+        return (
+               <Button onPress={this.onButtonPress.bind(this)}>
+                        Log in    
+               </Button>  
+        );
     }
 
     render() {
@@ -46,9 +58,7 @@ class LoginForm extends Component {
                     {this.state.error}
                 </Text>
                 <CardSection>
-                    <Button onPress={this.onButtonPress.bind(this)}>
-                        Log in    
-                    </Button> 
+                    {this.renderButton()}
                 </CardSection>
             </Card>
 
